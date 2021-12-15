1036766014804e5bb37739ae9c26fe71772e84b6
Andriy Tysiak <tysiak45@gmail.com>
2021-12-15 22:47:58 +0200

    formatted code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,19 +7,19 @@ public class Main {
     a*(x+x)/y-a*3
     a+b/x-y
     X-e/2%e+1 */
-        int x =1;
-        double a =1.1;
-        int y =2;
+        int x = 1;
+        double a = 1.1;
+        int y = 2;
         double w = 3.3;
         double e = 0.13;
         short b = 6;
-        var resaltOne =x+y-a/2*(x+w);
-        var resaltTwo =a*(x+x)/y-a*3;
-        var resaltThree =a+b/x-y;
-        var resaltFour =x-e/2%e+1;
-        System.out.println("Значення виразу x+y-a/2*(x+w):\n"+resaltOne);
-        System.out.println("Значення виразу a*(x+x)/y-a*3:\n"+resaltTwo);
-        System.out.println("Значення виразу a+b/x-y:\n"+resaltThree);
-        System.out.println("Значення виразу x-e/2%e+1:\n"+resaltFour);
+        var resaltOne = x + y - a / 2 * (x + w);
+        var resaltTwo = a * (x + x) / y - a * 3;
+        var resaltThree = a + b / x - y;
+        var resaltFour = x - e / 2 % e + 1;
+        System.out.println("Значення виразу x+y-a/2*(x+w):\n" + resaltOne);
+        System.out.println("Значення виразу a*(x+x)/y-a*3:\n" + resaltTwo);
+        System.out.println("Значення виразу a+b/x-y:\n" + resaltThree);
+        System.out.println("Значення виразу x-e/2%e+1:\n" + resaltFour);
     }
 }
